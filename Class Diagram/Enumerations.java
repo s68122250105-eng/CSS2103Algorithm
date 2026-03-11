0c235a8e3d4287b83d6381eaf37898ea4c1bbafd
s68122250105-eng <s68122250105@ssru.ac.th>
2026-03-11 22:04:08 +0700

    Add files via upload

---
+++ Class Diagram/Enumerations.java
@@ -0,0 +1,18 @@
+enum Gender {
+    M, F
+}
+
+
+enum RoleUser {
+    PublicRelation, Division, Manager
+}
+
+
+enum StatusProposal {
+    Waiting, Rejected, Accepted;
+
+    public void changeStatus() {
+    
+    }
+
+}
